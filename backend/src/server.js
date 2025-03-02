5a63dd8a9b6c6f25bbcabcd6dcad2901a747dfe8
CHARAN PEDDI <charanpeddi37@gmail.com>
2025-03-02 16:18:06 +0530

    SERVER PORT

--- backend/src/server.js
+++ backend/src/server.js
@@ -72,7 +72,7 @@ app.use("/api/quizzes", quizRoutes);
 app.use("/api/admin", adminRoutes);
 
 // Start Server
-const PORT = process.env.PORT || 2424;
+const PORT = process.env.PORT || 1310;
 const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Graceful Shutdown
